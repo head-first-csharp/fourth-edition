33dc8056e60156fc3a3efcae5b49b8ffe36d8c6b
Jon Galloway <jongalloway@gmail.com>
2020-10-15 14:20:11 -0700

    Updated to .NET 5.0 RC2

--- Code/Chapter_1/BlazorMatchGame/BlazorMatchGame/Program.cs
+++ Code/Chapter_1/BlazorMatchGame/BlazorMatchGame/Program.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.Extensions.Logging;
 namespace BlazorMatchGame
 {
     public class Program
@@ -12,9 +14,9 @@ namespace BlazorMatchGame
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
