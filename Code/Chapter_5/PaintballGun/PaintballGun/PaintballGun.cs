f97ac4b4c1c589d992263005fb6020de0354fdcd
Andrew Stellman <astellman@stellman-greene.com>
2020-11-05 17:28:25 -0500

    Replace MachineGun with PaintballGun

---
+++ Code/Chapter_5/PaintballGun/PaintballGun/PaintballGun.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaintballGun
+{
+    class PaintballGun
+    {
+        public int MagazineSize { get; private set; } = 16;
+
+        private int balls = 0;
+        public int BallsLoaded { get; private set; }
+
+        public bool IsEmpty() { return BallsLoaded == 0; }
+
+        public int Balls
+        {
+            get { return balls; }
+
+            set
+            {
+                if (value > 0)
+                    balls = value;
+                Reload();
+            }
+        }
+
+        public void Reload()
+        {
+            if (balls > MagazineSize)
+                BallsLoaded = MagazineSize;
+            else
+                BallsLoaded = balls;
+        }
+
+        public bool Shoot()
+        {
+            if (BallsLoaded == 0) return false;
+            BallsLoaded--;
+            balls--;
+            return true;
+        }
+
+        public PaintballGun(int balls, int magazineSize, bool loaded)
+        {
+            this.balls = balls;
+            MagazineSize = magazineSize;
+            if (!loaded) Reload();
+        }
+    }
+}
---
+++ Code/Chapter_5/PaintballGun/PaintballGun/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PaintballGun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfBalls = ReadInt(20, "Number of balls");
+            int magazineSize = ReadInt(16, "Magazine size");
+
+            Console.Write($"Loaded [false]: ");
+            bool.TryParse(Console.ReadLine(), out bool isLoaded);
+
+            PaintballGun gun = new PaintballGun(numberOfBalls, magazineSize, isLoaded);
+            while (true)
+            {
+                Console.WriteLine($"{gun.Balls} balls, {gun.BallsLoaded} loaded");
+                if (gun.IsEmpty()) Console.WriteLine("WARNING: You're out of ammo");
+                Console.WriteLine("Space to shoot, r to reload, + to add ammo, q to quit");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == ' ') Console.WriteLine($"Shooting returned {gun.Shoot()}");
+                else if (key == 'r') gun.Reload();
+                else if (key == '+') gun.Balls += gun.MagazineSize;
+                else if (key == 'q') return;
+            }
+
+            static int ReadInt(int lastUsedValue, string prompt)
+            {
+                Console.Write(prompt + " [" + lastUsedValue + "]: ");
+                string line = Console.ReadLine();
+                if (int.TryParse(line, out int value))
+                {
+                    Console.WriteLine("   using value " + value);
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine("   using default value " + lastUsedValue);
+                    return lastUsedValue;
+                }
+            }
+        }
+
+    }
+}
