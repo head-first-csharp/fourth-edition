7def59065553563ac042d52668188355049f4240
Andrew Stellman <astellman@stellman-greene.com>
2020-11-07 16:18:02 -0500

    Updated BlazorMatchGame

--- Code/Chapter_1/BlazorMatchGame/BlazorMatchGame/Program.cs
+++ Code/Chapter_1/BlazorMatchGame/BlazorMatchGame/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+
 namespace BlazorMatchGame
 {
     public class Program
@@ -14,7 +15,7 @@ namespace BlazorMatchGame
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
