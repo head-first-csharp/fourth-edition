326e1e7ebdf5504bc4e491ab1048485a22d981aa
Andrew Stellman <astellman@stellman-greene.com>
2020-11-07 15:56:10 -0500

    Updated ExperimentWithControlsBlazor

--- Code/Chapter_2/ExperimentWithControlsBlazor/ExperimentWithControlsBlazor/Program.cs
+++ Code/Chapter_2/ExperimentWithControlsBlazor/ExperimentWithControlsBlazor/Program.cs
@@ -1,12 +1,12 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExperimentWithControlsBlazor
 {
@@ -15,7 +15,7 @@ namespace ExperimentWithControlsBlazor
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
