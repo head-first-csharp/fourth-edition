b54ed35b7261978c6cb26c23308d65cbb808e774
Andrew Stellman <astellman@stellman-greene.com>
2020-11-07 15:01:36 -0500

    Added SloppyJoeBlazor

---
+++ Code/Chapter_4/SloppyJoeBlazor/SloppyJoeBlazor/MenuItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace SloppyJoeBlazor
+{
+    class MenuItem
+    {
+        public Random Randomizer = new Random();
+        public string[] Proteins = { "Roast beef", "Salami", "Turkey",
+            "Ham", "Pastrami", "Tofu" };
+        public string[] Condiments = { "yellow mustard", "brown mustard",
+           "honey mustard", "mayo", "relish", "french dressing" };
+        public string[] Breads = { "rye", "white", "wheat", "pumpernickel", "a roll" };
+
+        public string Description = "";
+        public string Price;
+
+        public void Generate()
+        {
+            string randomProtein = Proteins[Randomizer.Next(Proteins.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            Description = randomProtein + " with " + randomCondiment + " on " + randomBread;
+
+            decimal bucks = Randomizer.Next(2, 5);
+            decimal cents = Randomizer.Next(1, 98);
+            decimal price = bucks + (cents * .01M);
+            Price = price.ToString("c");
+        }
+    }
+
+}
