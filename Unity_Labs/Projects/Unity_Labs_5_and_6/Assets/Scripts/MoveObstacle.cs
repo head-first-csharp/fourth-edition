69c1d2cb412ace8680df7e4755158f57f6ff54ac
Andrew Stellman <astellman@stellman-greene.com>
2020-11-10 11:04:55 -0500

    Added Unity labs 5 and 6

---
+++ Unity_Labs/Projects/Unity_Labs_5_and_6/Assets/Scripts/MoveObstacle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObstacle : MonoBehaviour
+{
+    void OnMouseDrag()
+    {
+        transform.position += new Vector3(0, Input.GetAxis("Mouse Y"), 0);
+        if (transform.position.y < 1)
+        {
+            transform.position = new Vector3(transform.position.x, 1, transform.position.z);
+        }
+        if (transform.position.y > 5)
+        {
+            transform.position = new Vector3(transform.position.x, 5, transform.position.z);
+        }
+    }
+}
+
---
+++ Unity_Labs/Projects/Unity_Labs_5_and_6/Assets/Scripts/MoveToClick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToClick : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Camera cameraComponent = GameObject.Find("Main Camera").GetComponent<Camera>();
+            Ray ray = cameraComponent.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.collider.gameObject.tag != "Obstacle")
+                {
+                    agent.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
+
