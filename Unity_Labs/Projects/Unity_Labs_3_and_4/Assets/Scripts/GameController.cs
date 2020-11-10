1584c73b1c85eb0a8138a6574eaa7d32a6411ef1
Andrew Stellman <astellman@stellman-greene.com>
2020-11-10 11:04:24 -0500

    Added Unity Labs 3 and 4

---
+++ Unity_Labs/Projects/Unity_Labs_3_and_4/Assets/Scripts/GameController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject OneBallPrefab;
+    public int Score = 0;
+    public bool GameOver = true;
+    public int NumberOfBalls = 0;
+    public int MaximumBalls = 15;
+    public Text ScoreText;
+    public Button PlayAgainButton;
+
+    void Start()
+    {
+        InvokeRepeating("AddABall", 1.5F, 1);
+    }
+
+    void AddABall()
+    {
+        if (!GameOver)
+        {
+            Instantiate(OneBallPrefab);
+            NumberOfBalls++;
+            if (NumberOfBalls >= MaximumBalls)
+            {
+                GameOver = true;
+                PlayAgainButton.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    public void ClickedOnBall()
+    {
+        Score++;
+        NumberOfBalls--;
+    }
+
+    public void StartGame()
+    {
+        foreach (GameObject ball in GameObject.FindGameObjectsWithTag("GameController"))
+        {
+            Destroy(ball);
+        }
+        PlayAgainButton.gameObject.SetActive(false);
+        Score = 0;
+        NumberOfBalls = 0;
+        GameOver = false;
+    }
+
+    void Update()
+    {
+        ScoreText.text = Score.ToString();
+    }
+
+}
---
+++ Unity_Labs/Projects/Unity_Labs_3_and_4/Assets/Scripts/OneBallBehaviour.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;public class OneBallBehaviour : MonoBehaviour{    public float XSpeed;    public float YSpeed;    public float ZSpeed;    public float Multiplier = 0.75F;    public float TooFar = 5;    static int BallCount = 0;    public int BallNumber;
+
+    // Start is called before the first frame update
+    void Start()    {        BallCount++;        BallNumber = BallCount;        ResetBall();    }
+
+    // Update is called once per frame
+    void Update()    {        transform.Translate(Time.deltaTime * XSpeed,                            Time.deltaTime * YSpeed, Time.deltaTime * ZSpeed);        XSpeed += Multiplier - Random.value * Multiplier * 2;        YSpeed += Multiplier - Random.value * Multiplier * 2;        ZSpeed += Multiplier - Random.value * Multiplier * 2;        if ((Mathf.Abs(transform.position.x) > TooFar)
+             || (Mathf.Abs(transform.position.y) > TooFar)             || (Mathf.Abs(transform.position.z) > TooFar))        {            ResetBall();        }    }    void ResetBall()    {        XSpeed = Random.value * Multiplier;        YSpeed = Random.value * Multiplier;        ZSpeed = Random.value * Multiplier;        transform.position = new Vector3(3 - Random.value * 6,            3 - Random.value * 6, 3 - Random.value * 6);    }    void OnMouseDown()    {        GameController controller = Camera.main.GetComponent<GameController>();        if (!controller.GameOver)        {            controller.ClickedOnBall();            Destroy(gameObject);        }    }}
