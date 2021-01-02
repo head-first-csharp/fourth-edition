97adb85c818390866c490e5ab1b18c90271cb00b
Andrew Stellman <astellman@stellman-greene.com>
2021-01-02 11:31:54 -0500

    Updated GoFish in Chapter 9 to move MockRandom to a separate file

--- Code/Chapter_9/GoFish/GoFishTests/PlayerTests.cs
+++ Code/Chapter_9/GoFish/GoFishTests/PlayerTests.cs
@@ -122,14 +122,4 @@ namespace GoFishTests
         }
     }
 
-    /// <summary>
-    /// Mock Random for testing that always returns a specific value
-    /// </summary>
-    public class MockRandom : System.Random
-    {
-        public int ValueToReturn { get; set; } = 0;
-        public override int Next() => ValueToReturn;
-        public override int Next(int maxValue) => ValueToReturn;
-        public override int Next(int minValue, int maxValue) => ValueToReturn;
-    }
 }
