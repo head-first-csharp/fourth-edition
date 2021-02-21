2d7e5351c3ff3bbc5891044bf0da6882e3df2c8b
Andrew Stellman <astellman@stellman-greene.com>
2021-02-21 11:31:30 -0500

    Updated Unity Lab 8 projects

---
+++ Unity_Labs/Projects/Sleep_Experiment/Assets/WakeUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WakeUp : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Debug.Log($"Sleeping: {rigidBody.IsSleeping()}  Time: {Time.time:0.00}");
+    }
+
+    void OnMouseDown()
+    {
+        rigidBody.WakeUp();
+    }
+}
