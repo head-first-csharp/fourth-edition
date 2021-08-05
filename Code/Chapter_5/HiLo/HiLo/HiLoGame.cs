bb2ff1176cf1b138be74ebcfa2d3b432ea0bff4b
Andrew Stellman <astellman@stellman-greene.com>
2021-08-05 13:59:47 -0500

    Added corrected HiLo exercise in Chapter 5

--- Code/Chapter_5/HiLo/HiLo/HiLoGame.cs
+++ Code/Chapter_5/HiLo/HiLo/HiLoGame.cs
@@ -4,18 +4,24 @@ using System.Text;
 
 namespace HiLo
 {
+    /*
+     * In some early versions of Head First C# (4th edition) there was a minor technical issue
+     * with this exercise. This code corresponds to the updated version, which you can find here:
+     * https://github.com/head-first-csharp/fourth-edition/raw/master/Additional_material/Corrected_pages_243_and_244.pdf
+     */
+
     static class HiLoGame
     {
         public const int MAXIMUM = 10;
         private static Random random = new Random();
         private static int currentNumber = random.Next(1, MAXIMUM + 1);
+        private static int nextNumber = random.Next(1, MAXIMUM + 1);
         private static int pot = 10;
 
         public static int GetPot() { return pot; }
 
         public static void Guess(bool higher)
         {
-            int nextNumber = random.Next(1, MAXIMUM + 1);
             if ((higher && nextNumber >= currentNumber) ||
                     (!higher && nextNumber <= currentNumber))
             {
@@ -28,15 +34,16 @@ namespace HiLo
                 pot--;
             }
             currentNumber = nextNumber;
+            nextNumber = random.Next(1, MAXIMUM + 1);
             Console.WriteLine($"The current number is {currentNumber}");
         }
 
         public static void Hint()
         {
             int half = MAXIMUM / 2;
-            if (currentNumber >= half)
-                Console.WriteLine($"The number is at least {half}");
-            else Console.WriteLine($"The number is at most {half}");
+            if (nextNumber >= half)
+                Console.WriteLine($"The current number is {currentNumber}, the next number is at least {half}");
+            else Console.WriteLine($"The current number is {currentNumber}, the next is at most {half}");
             pot--;
         }
     }
